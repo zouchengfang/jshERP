46ca40ef4bb86b72dce288ee8021c6b2c19db755
zouchengfang <haotian2008@vip.qq.com>
2020-09-30 16:38:44 +0800

    444

--- src/main/bin/run-manage.sh
+++ src/main/bin/run-manage.sh
@@ -4,15 +4,16 @@ readonly APP_HOME=${FILE_PATH:-$(dirname $(cd `dirname $0`; pwd))}/jshERP2.0
 
 #readonly JAVA_HOME=""
 
-readonly CONFIG_HOME="$APP_HOME/config/application.properties"
+readonly CONFIG_HOME="$APP_HOME/config"
 readonly LIB_HOME="$APP_HOME/lib"
 readonly LOGS_HOME="$APP_HOME/logs"
 
 readonly PID_FILE="$LOGS_HOME/application.pid"
 readonly APP_MAIN_CLASS="jshERP2.0.jar"
 readonly LOG_CONFIG="$CONFIG_HOME/logback-spring.xml"
+readonly POP_FILE="$CONFIG_HOME/application.properties"
 
-readonly JAVA_RUN="-Dlogs.home=$LOGS_HOME -Dlogging.config=$LOG_CONFIG -Dspring.config.location=file:$CONFIG_HOME -Dspring.pid.file=$PID_FILE -Dspring.pid.fail-on-write-error=true"
+readonly JAVA_RUN="-Dlogs.home=$LOGS_HOME -Dlogging.config=$LOG_CONFIG -Dspring.config.location=file:$POP_FILE -Dspring.pid.file=$PID_FILE -Dspring.pid.fail-on-write-error=true"
 readonly JAVA_OPTS="-server -Xms128m -Xmx320m -XX:PermSize=128M -XX:MaxPermSize=256M $JAVA_RUN"
 
 readonly JAVA="java"
